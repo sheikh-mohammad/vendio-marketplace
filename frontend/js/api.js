4246f3d5e5daaef51e8da566f638b46f503307cf
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-08-25 19:54:03 +0500

    Updated prod url

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,5 +1,5 @@
 const hostname = window.location.hostname;
-const isProd = hostname === "vendio-marketplace.vercel.app";
+const isProd = hostname === "vendio-marketplace.netlify.app";
 
 let API_BASE;
 if (isProd) {
